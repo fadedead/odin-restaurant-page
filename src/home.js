e1874fc910299d2cfa64d294155a0eb1a14a5168
fadedead <fadedeadd@gmail.com>
2023-12-05 08:05:44 +0530

    Added: Home page

--- src/home.js
+++ src/home.js
@@ -1,23 +1,39 @@
 import './style.css';
-import Book from './components/content/book.svg';
+import BookImage from './components/content/book.svg';
 
 export default function getHome() {
     // Create the home container
-    let home = document.createElement('div');
+    const home = document.createElement('div');
     home.classList.add('home');
 
+    // Create the book div
+    const book = document.createElement('div');
+    book.classList.add('book');
+
     // Create the book image
-    let book = new Image();
-    book.src = Book;
-    book.classList.add('page');
+    const bookImg = new Image();
+    bookImg.src = BookImage;
+    bookImg.classList.add('page');
 
-    // Append the book image to the home container
+    // Create a container for the text
+    const aboutContainer = document.createElement('div');
+    aboutContainer.classList.add('about-container');
+
+    // Text for the about page
+    const aboutText = document.createElement('p');
+    aboutText.innerHTML = `
+    Welcome to The Logg, your Minecraft-inspired dining escape! Our menu blends real-world flavors with pixelated delights, crafted by passionate chefs who celebrate the creativity of Minecraft. Step into our themed haven, where pixelated landscapes and cozy ambiance create a unique dining adventure. Whether you're a gaming enthusiast or seeking a unique experience, join us at The Logg, where every bite is a celebration of Minecraft's wonders. Welcome to the intersection of gaming and gourmet!`;
+    aboutText.classList.add('about');
+
+    // Append the text to the container
+    aboutContainer.appendChild(aboutText);
+
+    // Append the image and text container to the book div
+    book.appendChild(bookImg);
+    book.appendChild(aboutContainer);
+
+    // Append the book div to the home container
     home.appendChild(book);
-
-    // Create other elements (caption, timings, location)
-    let caption = document.createElement('div');
-    let timings = document.createElement('div');
-    let location = document.createElement('div');
 
     return home;
 }
